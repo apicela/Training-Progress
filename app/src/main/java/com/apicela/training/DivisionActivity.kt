be1268616576b4ab66f6bea691b3b3c283466dd4
apicela <jamilnetobr@gmail.com>
2024-03-01 17:24:41 -0300

    ok

---
+++ app/src/main/java/com/apicela/training/DivisionActivity.kt
@@ -0,0 +1,11 @@
+package com.apicela.training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DivisionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_division)
+    }
+}
--- app/src/main/java/com/apicela/training/services/WorkoutService.kt
+++ app/src/main/java/com/apicela/training/services/WorkoutService.kt
@@ -1,12 +1,17 @@
 package com.apicela.training.services
 
 import android.content.Context
+import com.apicela.training.models.Division
 import com.apicela.training.models.Workout
 import com.apicela.training.utils.DataManager
 
 class WorkoutService(context: Context) {
     fun addWorkoutToList(workoutName: String, descricao: String) {
-        val workoutItem = Workout(workoutName, descricao, listOf())
+        val workoutItem = Workout(workoutName, descricao, listOf(
+            Division("A", listOf()),
+            Division("B", listOf()),
+            Division("C", listOf())
+            ))
         val lista = DataManager.loadWorkoutItems()
         lista.add(workoutItem)
         DataManager.saveWorkoutItems(lista)
