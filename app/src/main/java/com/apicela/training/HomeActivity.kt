1dc438777484a6a6f75d9043d44369bc75ba11cf
apicela <jamilnetobr@gmail.com>
2024-07-14 21:52:49 -0300

    nav visual

--- app/src/main/java/com/apicela/training/HomeActivity.kt
+++ app/src/main/java/com/apicela/training/HomeActivity.kt
@@ -3,9 +3,12 @@ package com.apicela.training
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.apicela.training.data.DataManager
 import com.apicela.training.data.Database
 import com.apicela.training.utils.Codes
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     companion object {
@@ -16,6 +19,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         DATABASE = DataManager.getDatabase(applicationContext)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.navigation_bar)
+        val navController = Navigation.findNavController(this,R.id.frag_host)
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+
 
     }
 
