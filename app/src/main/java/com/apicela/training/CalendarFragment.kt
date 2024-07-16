e14e99e356709ce43318327fdc2cc4b9efbd77a9
apicela <jamilnetobr@gmail.com>
2024-07-15 22:03:58 -0300

    nav visual

--- app/src/main/java/com/apicela/training/CalendarFragment.kt
+++ app/src/main/java/com/apicela/training/CalendarFragment.kt
@@ -1,50 +1,69 @@
-//package com.apicela.training
-//
-//import android.os.Bundle
-//import android.widget.ArrayAdapter
-//import android.widget.CalendarView
-//import android.widget.ListView
-//import androidx.fragment.app.Fragment
-//import com.apicela.training.services.ExecutionService
-//import kotlinx.coroutines.runBlocking
-//import java.util.Calendar
-//import java.util.Date
-//
-//class CalendarFragment : Fragment() {
-//
-//    private lateinit var calendarView: CalendarView
-//    private lateinit var listView: ListView
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        val executionService: ExecutionService = ExecutionService(HomeActivity.database)
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.calendar_activity)
-//        backButton = findViewById(R.id.back_button)
-//
-//        backButton.setOnClickListener {
-//            finish()
-//        }
-//        calendarView = findViewById(R.id.calendarView)
-//        listView = findViewById(R.id.listView)
-//
-//        val today = Date()
-//        val listOfExecutions =
-//            runBlocking { executionService.groupExercisesExecutionByDateIntoString(today) }
-//        val adapter = ArrayAdapter(this, R.layout.simple_list_item, listOfExecutions)
-//        listView.adapter = adapter
-//
-//        // Configurar o ouvinte de clique no calendário
-//        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-//            adapter.clear()
-//            val clickedDate = Calendar.getInstance()
-//            clickedDate.set(year, month, dayOfMonth)
-//            val date: Date = (clickedDate.time)
-//            val listOfExecutions =
-//                runBlocking { executionService.groupExercisesExecutionByDateIntoString(date) }
-//            adapter.addAll(listOfExecutions)
-//            adapter.notifyDataSetChanged()
-//        }
-//    }
-//
-//
-//}
+package com.apicela.training
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CalendarView
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import com.apicela.training.services.ExecutionService
+import kotlinx.coroutines.runBlocking
+import java.util.Calendar
+import java.util.Date
+
+class CalendarFragment : Fragment(R.layout.fragment_calendar) {
+
+    private lateinit var calendarView: CalendarView
+    private lateinit var listView: ListView
+    private lateinit var adapter : ArrayAdapter<String>
+    private val executionService: ExecutionService = ExecutionService(HomeActivity.DATABASE)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+        linkViewFields(view)
+        return view;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        calendarTodayInfo()
+        calendarOnClickGetInfo()
+    }
+
+    private fun calendarOnClickGetInfo() {
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            adapter.clear()
+            val clickedDate = Calendar.getInstance()
+            clickedDate.set(year, month, dayOfMonth)
+            val date: Date = (clickedDate.time)
+            val listOfExecutions =
+                runBlocking { executionService.groupExercisesExecutionByDateIntoString(date) }
+            adapter.addAll(listOfExecutions)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun calendarTodayInfo() {
+        val today = Date()
+        val listOfExecutions =
+            runBlocking { executionService.groupExercisesExecutionByDateIntoString(today) }
+        adapter = ArrayAdapter(requireContext(), R.layout.simple_list_item, listOfExecutions)
+        listView.adapter = adapter
+    }
+
+    private fun linkViewFields(v : View) {
+        calendarView = v.findViewById(R.id.calendarView)
+        listView = v.findViewById(R.id.listView)
+    }
+
+
+}
